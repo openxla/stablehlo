e708e55075afd7a7f2d817a34188c72946ec6810
Kevin Gleason <gleasonk@google.com>
2022-09-19 16:28:42 +0000

    Replace uses of __PRETTY_FUNCTION__ with LLVM_PRETTY_FUNCTION

--- stablehlo/dialect/ChloBytecode.cpp
+++ stablehlo/dialect/ChloBytecode.cpp
@@ -16,6 +16,7 @@ limitations under the License.
 #include "stablehlo/dialect/ChloBytecode.h"
 
 #include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/Compiler.h"
 #include "llvm/Support/Debug.h"
 #include "mlir/Bytecode/BytecodeImplementation.h"
 #include "mlir/IR/Diagnostics.h"
@@ -35,18 +36,18 @@ limitations under the License.
 #define _EXTRACT_AFTER(a, b) \
   llvm::StringRef(a).substr(llvm::StringRef(a).find(b))
 
-#define _LOG_CALL_TO(func)                                                    \
-  DEBUG_WITH_TYPE(                                                            \
-      "chlo-bytecode",                                                        \
-      llvm::errs() << "Called: " << _EXTRACT_AFTER(__PRETTY_FUNCTION__, func) \
-                   << '\n')
+#define _LOG_CALL_TO(func)                                                     \
+  DEBUG_WITH_TYPE("chlo-bytecode",                                             \
+                  llvm::errs()                                                 \
+                      << "Called: "                                            \
+                      << _EXTRACT_AFTER(LLVM_PRETTY_FUNCTION, func) << '\n')
 
 #define LOG_WRITE_CALL _LOG_CALL_TO("write")
 #define LOG_READ_CALL _LOG_CALL_TO(__func__)
-#define LOG_NOT_IMPLEMENTED \
-  DEBUG_WITH_TYPE(          \
-      "chlo-bytecode",      \
-      llvm::errs() << "***Not Implemented: " << __PRETTY_FUNCTION__ << '\n')
+#define LOG_NOT_IMPLEMENTED                                                    \
+  DEBUG_WITH_TYPE("chlo-bytecode", llvm::errs()                                \
+                                       << "***Not Implemented: "               \
+                                       << LLVM_PRETTY_FUNCTION << '\n')
 
 //===----------------------------------------------------------------------===//
 // Encoding
