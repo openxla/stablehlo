de7652205e016ed936f42cddc3ab35e7f711599a
Smit Hinsu <hinsu@google.com>
2022-12-02 01:08:02 -0800

    Fix verifier check disallowing dynamic shapes in reduce family ops

--- stablehlo/dialect/TypeInference.cpp
+++ stablehlo/dialect/TypeInference.cpp
@@ -482,8 +482,9 @@ LogicalResult verifyReducerShape(
          outputShapeIdx < static_cast<int64_t>(allowedDimensions.size()) &&
          argShapeIdx < static_cast<int64_t>(argShape.size());
          outputShapeIdx++)
-      if (allowedDimensions[outputShapeIdx] == argShape[argShapeIdx] ||
-          argShape[argShapeIdx] == ShapedType::kDynamic)
+      if (allowedDimensions[outputShapeIdx] == ShapedType::kDynamic ||
+          argShape[argShapeIdx] == ShapedType::kDynamic ||
+          allowedDimensions[outputShapeIdx] == argShape[argShapeIdx])
         argShapeIdx++;
 
     if (argShapeIdx != static_cast<int64_t>(argShape.size()))
