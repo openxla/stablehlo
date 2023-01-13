2ef2509f89adcc3dc24b83f57bf28458c8d4c28f
Gunhyun Park <gunhyun@google.com>
2023-01-13 19:11:18 +0000

    Address feedback

--- stablehlo/dialect/TypeInference.cpp
+++ stablehlo/dialect/TypeInference.cpp
@@ -3583,15 +3583,11 @@ LogicalResult verifyReverseOp(Optional<Location> location, Value operand,
   auto dims = dimensions.getValues<int64_t>();
   llvm::SmallDenseSet<int64_t> uniqueDims(dims.begin(), dims.end());
   if (uniqueDims.size() != dims.size())
-    return emitOptionalError(
-        location, "dimensions attribute values should be unique. Got: ", dims);
+    return emitOptionalError(location, "dimensions should be unique. Got: ", dims);
   auto operandTy = operand.getType().dyn_cast<RankedTensorType>();
-  if (!operandTy) return success();
   for (int64_t dim : uniqueDims) {
-    if (dim < 0 || dim >= operandTy.getRank())
-      return emitOptionalError(
-          location, "all dimension values should be between [0, ",
-          operandTy.getRank(), "). Got dimension: ", dim, ".");
+    if (dim < 0 || (operandTy && dim >= operandTy.getRank()))
+      return emitOptionalError(location, "all dimensions should be between [0, ", operandTy.getRank(), "). Got dimension: ", dim, ".");
   }
   return success();
 }
