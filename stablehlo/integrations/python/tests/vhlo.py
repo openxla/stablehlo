0cc5f028b5b3292996cf9825807a82bef426e275
Kevin Gleason <gleasonk@google.com>
2023-02-10 21:49:32 +0000

    Address feedback - update test name

--- stablehlo/integrations/python/tests/vhlo.py
+++ stablehlo/integrations/python/tests/vhlo.py
@@ -1,5 +1,5 @@
 # Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-# Copyright 2022 The StableHLO Authors.
+# Copyright 2023 The StableHLO Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for CHLO Python APIs."""
+"""Tests for VHLO Python APIs."""
 
 # pylint: disable=wildcard-import,undefined-variable
 
@@ -28,7 +28,7 @@ def run(f):
   return f
 
 @run
-def test_asm_parse():
+def test_parse():
   asm = """
     vhlo.func @main() -> () {
       vhlo.return 
