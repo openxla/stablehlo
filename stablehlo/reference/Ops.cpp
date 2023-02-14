bbcf61b9beef04186b2d5ee0b9f1298a1479dcb6
Xin Zhou <zhouxin@google.com>
2023-02-14 13:43:05 -0800

    Add tests and refine comments

--- stablehlo/reference/Ops.cpp
+++ stablehlo/reference/Ops.cpp
@@ -96,12 +96,10 @@ Tensor evalClampOp(const Tensor &min, const Tensor &operand, const Tensor &max,
                    Type resultType) {
   Tensor result(resultType);
   for (auto it = result.index_begin(); it != result.index_end(); ++it) {
-    Element minElement = min.getType().getRank() != 0
-                             ? min.get(*it)
-                             : min.get(*(min.index_begin()));
-    Element maxElement = min.getType().getRank() != 0
-                             ? max.get(*it)
-                             : max.get(*(max.index_begin()));
+    Element minElement =
+        min.getType().getRank() != 0 ? min.get(*it) : min.get({});
+    Element maxElement =
+        max.getType().getRank() != 0 ? max.get(*it) : max.get({});
     result.set(*it, stablehlo::min(stablehlo::max(operand.get(*it), minElement),
                                    maxElement));
   }
