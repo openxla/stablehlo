b9174cb715ce40946a3823769ee122ba96daed25
Kevin Gleason <gleasonk@google.com>
2023-03-21 21:54:37 +0000

    fix copyrights

--- stablehlo/integrations/python/StablehloModule.cpp
+++ stablehlo/integrations/python/StablehloModule.cpp
@@ -1,5 +1,5 @@
 /* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-   Copyright 2022 The StableHLO Authors.
+   Copyright 2023 The StableHLO Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
