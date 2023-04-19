8432e74322435cd6833f2d1faae1af7e90d1e7e8
Kevin Gleason <gleasonk@google.com>
2023-04-19 20:48:06 +0000

    Fix llvm commit lint script

--- build_tools/github_actions/lint_llvm_commit.sh
+++ build_tools/github_actions/lint_llvm_commit.sh
@@ -76,10 +76,13 @@ print_autofix() {
 }
 
 update_llvm_commit_and_sha256() {
+  # Update WORKSPACE commit hash to match llvm_version.txt
   LLVM_COMMIT=$(llvm_commit_from_version_txt)
-  LLVM_SHA256=$(llvm_sha256_from_archive)
   echo "Bumping commit to: $LLVM_COMMIT"
   sed -i '/^LLVM_COMMIT/s/"[^"]*"/"'$LLVM_COMMIT'"/g' $PATH_TO_WORKSPACE
+
+  # Now update WORKSPACE SHA256 using the newly updated commit hash
+  LLVM_SHA256=$(llvm_sha256_from_archive)
   echo "Bumping sha256 to: $LLVM_SHA256"
   sed -i '/^LLVM_SHA256/s/"[^"]*"/"'$LLVM_SHA256'"/g' $PATH_TO_WORKSPACE
 }
