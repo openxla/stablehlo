fef6dee54cf3a08d6634f6704adecba7d21a93aa
Karthik Rangasai <karthikrangasai@gmail.com>
2023-04-22 15:01:33 +0530

    Changes following PR review.

--- stablehlo/reference/Element.cpp
+++ stablehlo/reference/Element.cpp
@@ -615,16 +615,14 @@ Element ceil(const Element &el) {
   return Element(el.getType(), val);
 }
 
-std::complex<double> complex(const Element &e1, const Element &e2) {
-  auto e1Type = e1.getType();
-  auto e2Type = e2.getType();
-  if (isSupportedFloatType(e1Type) && isSupportedFloatType(e2Type)) {
-    return std::complex<double>(e1.getFloatValue().convertToDouble(),
-                                e2.getFloatValue().convertToDouble());
-  }
-  report_fatal_error(invalidArgument("Unsupported element types: %s, %s",
-                                     debugString(e1Type).c_str(),
-                                     debugString(e2Type).c_str()));
+Element complex(const Element &e1, const Element &e2) {
+  auto type = e1.getType();
+  auto complexType = ComplexType::get(type);
+  if (isSupportedComplexType(complexType))
+    return Element(complexType, std::complex<APFloat>(e1.getFloatValue(),
+                                                      e2.getFloatValue()));
+  report_fatal_error(invalidArgument("Unsupported element type: %s",
+                                     debugString(complexType).c_str()));
 }
 
 Element exponential(const Element &el) {
--- stablehlo/tests/interpret_complex.mlir
+++ stablehlo/tests/interpret_complex.mlir
@@ -1,18 +1,9 @@
 // RUN: stablehlo-translate --interpret -split-input-file %s
 
-func.func @add_op_test_f64() {
-  %0 = stablehlo.constant dense<[0.0, -0.0, 1.0, 0.125, 0.1, 3.14, 0x7FF0000000000000, 0.0]> : tensor<8xf64>
-  %1 = stablehlo.constant dense<[0.0, -0.0, 7.0, 0.75, 0.3, 3.14, 0.0, 0x7FF0000000000000]> : tensor<8xf64>
-  %2 = stablehlo.complex %0, %1 : tensor<8xcomplex<f64>>
-  check.expect_almost_eq_const %2, dense<[
-    (0.0, 0.0),
-    (-0.0, -0.0),
-    (1.0, 7.0),
-    (0.125, 0.75),
-    (0.1, 0.3),
-    (3.14, 3.14),
-    (0x7FF0000000000000, 0.0),
-    (0.0, 0x7FF0000000000000)
-  ]> : tensor<8xcomplex<f64>>
+func.func @complex_op_test_f64() {
+  %lhs = stablehlo.constant dense<[1.0, 3.0]> : tensor<2xf64>
+  %rhs = stablehlo.constant dense<[2.0, 4.0]> : tensor<2xf64>
+  %result = stablehlo.complex %lhs, %rhs : tensor<2xcomplex<f64>>
+  check.expect_almost_eq_const %result, dense<[(1.0, 2.0), (3.0, 4.0)]> : tensor<2xcomplex<f64>>
   func.return
 }
