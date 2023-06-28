1d95c9ba50a18386fce0b86caa0522dbcc21e4d6
Kevin Gleason <gleasonk@google.com>
2023-06-28 21:07:26 +0000

    Add test

--- stablehlo/transforms/PassPipelines.cpp
+++ stablehlo/transforms/PassPipelines.cpp
@@ -12,10 +12,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "stablehlo/transforms/Passes.h"
-
 #include "mlir/Pass/PassManager.h"
 #include "stablehlo/dialect/Version.h"
+#include "stablehlo/transforms/Passes.h"
 
 namespace mlir {
 namespace stablehlo {
