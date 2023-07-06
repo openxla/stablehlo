9b943ddbf9fd6795c6f1b96ba6cbf1a7245a4001
Kevin Gleason <gleasonk@google.com>
2023-07-06 04:00:18 +0000

    address feedback

--- build_tools/github_actions/lint_version.sh
+++ build_tools/github_actions/lint_version.sh
@@ -1,4 +1,4 @@
-# Copyright 2022 The StableHLO Authors.
+# Copyright 2023 The StableHLO Authors.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,8 +12,9 @@
 # limitations under the License.
 
 # This lint validates that there are compatibility tests for the current
-# version by extracting the current version from Version.h and checking if
-# files named stablehlo_legalize_to_vhlo.VERSION.mlir and .mlir.bc exist.
+# version by extracting the current version vX.Y.Z from Version.h and
+# checking if files named stablehlo_legalize_to_vhlo.X_Y_0.mlir and .mlir.bc
+# exist.
 
 ## Setup VERSION variable as global:
 VERSION_H="stablehlo/dialect/Version.h"
