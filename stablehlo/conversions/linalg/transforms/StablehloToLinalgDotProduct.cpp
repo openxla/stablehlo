3d3cc9aaafd179382b418b4c5519f69b5dad4be5
Kevin Gleason <gleasonk@google.com>
2023-11-16 00:25:18 +0000

    Update license to be consistent with StableHLO project

--- stablehlo/conversions/linalg/transforms/StablehloToLinalgDotProduct.cpp
+++ stablehlo/conversions/linalg/transforms/StablehloToLinalgDotProduct.cpp
@@ -1,12 +1,18 @@
-// Copyright 2019 The IREE Authors
-//
-// Licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+/* Copyright 2019 The IREE Authors
+   Copyright 2023 OpenXLA Authors. All Rights Reserved.
 
-// Implements logic for lowering StableHLO dot product ops to Linalg dialect.
-// These patterns are separated out to their own file to save on the compilation
-// times, given that we instantiate a large number of class templates here.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
 
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/SparseTensor/IR/SparseTensor.h"
