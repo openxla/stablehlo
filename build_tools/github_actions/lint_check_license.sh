8f78f6a1aef87142de3d614c0a4596a5c8acac6c
mlevesquedion <mlevesquedion@google.com>
2024-02-26 14:38:18 -0800

    Check copyright year and license type in license check (#2043)
    Addressing prior review feedback:
    https://github.com/openxla/stablehlo/pull/2018/files/001999860b0f236614b0e64493a447c9b0c85490#r1495173847

--- build_tools/github_actions/lint_check_license.sh
+++ build_tools/github_actions/lint_check_license.sh
@@ -76,14 +76,29 @@ for file in "${CHANGED_FILES[@]}"; do
     echo "Skipping file: $file"
     continue;
   fi
-  if ! head -20 "$file" | grep "Copyright" &>/dev/null; then
+  if ! head -20 "$file" | grep -Pzo '(?s)Copyright 20[1-9][0-9].*?Apache License, Version 2\.0' &>/dev/null; then
     UNLICENSED_FILES+=("$file")
   fi
 done
 
 if (( ${#UNLICENSED_FILES[@]} )); then
   echo "Found unlicensed files:
-$(printf "%s\n" "${UNLICENSED_FILES[@]}")"
+$(printf "%s\n" "${UNLICENSED_FILES[@]}")
+
+Please include the following at the top of the file(s):
+Copyright $(date +%Y) The StableHLO Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the \"License\");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an \"AS IS\" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License."
   exit 1
 fi
 
