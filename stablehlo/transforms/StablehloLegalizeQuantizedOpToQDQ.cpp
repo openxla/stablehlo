9c6335f9c66a58de1e14cdc4e4199e9efbccb1d0
Sandeep Dasgupta <sdasgup@google.com>
2024-06-17 19:45:21 +0000

    Address feedback I: refator macro with templatized function

--- stablehlo/transforms/StablehloLegalizeQuantizedOpToQDQ.cpp
+++ stablehlo/transforms/StablehloLegalizeQuantizedOpToQDQ.cpp
@@ -37,91 +37,50 @@ bool isAnyQuantizedTypes(TypeRange types) {
   });
 }
 
-#define DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(OpName)                            \
-  struct Quantized##OpName##Conversion : public OpRewritePattern<OpName> {     \
-    using OpRewritePattern<OpName>::OpRewritePattern;                          \
-    LogicalResult matchAndRewrite(OpName op,                                   \
-                                  PatternRewriter& rewriter) const override {  \
-      if (!isAnyQuantizedTypes(op->getOperandTypes()) &&                       \
-          !isAnyQuantizedTypes(op->getResultTypes())) {                        \
-        return failure();                                                      \
-      }                                                                        \
-                                                                               \
-      SmallVector<Value> dequantizedOperands;                                  \
-      for (auto operand : op->getOperands()) {                                 \
-        if (isa<quant::QuantizedType>(                                         \
-                getElementTypeOrSelf(operand.getType()))) {                    \
-          dequantizedOperands.push_back(                                       \
-              rewriter.create<UniformDequantizeOp>(op->getLoc(), operand));    \
-        } else {                                                               \
-          dequantizedOperands.push_back(operand);                              \
-        }                                                                      \
-      }                                                                        \
-                                                                               \
-      auto origOp = op.getOperation();                                         \
-      auto origAttrs = origOp->getAttrs();                                     \
-      auto newOp =                                                             \
-          rewriter.create<OpName>(op.getLoc(), dequantizedOperands, origAttrs) \
-              .getOperation();                                                 \
-                                                                               \
-      SmallVector<Value> quantizedResults;                                     \
-      for (auto [oldResult, newResult] :                                       \
-           llvm::zip(origOp->getResults(), newOp->getResults())) {             \
-        if (isa<quant::QuantizedType>(                                         \
-                getElementTypeOrSelf(oldResult.getType()))) {                  \
-          quantizedResults.push_back(                                          \
-              rewriter.create<stablehlo::UniformQuantizeOp>(                   \
-                  op->getLoc(), oldResult.getType(), newResult));              \
-        } else {                                                               \
-          quantizedResults.push_back(newResult);                               \
-        }                                                                      \
-      }                                                                        \
-      rewriter.replaceOp(op, quantizedResults);                                \
-      return success();                                                        \
-    }                                                                          \
-  };
+template <typename StablehloOpType>
+struct QuantizedStablehloOpConversion
+    : public OpRewritePattern<StablehloOpType> {
+  using OpRewritePattern<StablehloOpType>::OpRewritePattern;
+  LogicalResult matchAndRewrite(StablehloOpType op,
+                                PatternRewriter& rewriter) const override {
+    if (!isAnyQuantizedTypes(op->getOperandTypes()) &&
+        !isAnyQuantizedTypes(op->getResultTypes())) {
+      return failure();
+    }
 
-// The following list covers most of the operations which, according to the
-// stablehlo spoecification document, interprets the quantized
-// operation using dequant-op-quant strategy. The ones excluded are
-// AddOP, ConvolutionOp, DotGeneralOp, and DynamicConvOp, which are current
-// using `stablehlo-legalize-quant-to-int` pass for decomposituion to primitive
-// math operations.
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(AbsOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(Atan2Op)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(BatchNormGradOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(BatchNormInferenceOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(BatchNormTrainingOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(CbrtOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(CeilOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(CholeskyOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(ClampOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(CompareOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(CosineOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(DivOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(Expm1Op)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(ExpOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(FloorOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(Log1pOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(LogisticOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(LogOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(MaxOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(MinOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(MulOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(NegOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(PowOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(ReducePrecisionOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(RemOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(RoundOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(RoundNearestEvenOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(RsqrtOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(SelectOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(SignOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(SineOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(SqrtOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(SubtractOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(TanhOp)
-DEFINE_QUANT_REWRITE_TO_QDQ_PATTERN(TriangularSolveOp)
+    SmallVector<Value> dequantizedOperands;
+    for (auto operand : op->getOperands()) {
+      if (isa<quant::QuantizedType>(getElementTypeOrSelf(operand.getType()))) {
+        dequantizedOperands.push_back(
+            rewriter.create<UniformDequantizeOp>(op->getLoc(), operand));
+      } else {
+        dequantizedOperands.push_back(operand);
+      }
+    }
+
+    auto origOp = op.getOperation();
+    auto origAttrs = origOp->getAttrs();
+    auto newOp = rewriter
+                     .create<StablehloOpType>(op.getLoc(), dequantizedOperands,
+                                              origAttrs)
+                     .getOperation();
+
+    SmallVector<Value> quantizedResults;
+    for (auto [oldResult, newResult] :
+         llvm::zip(origOp->getResults(), newOp->getResults())) {
+      if (isa<quant::QuantizedType>(
+              getElementTypeOrSelf(oldResult.getType()))) {
+        quantizedResults.push_back(
+            rewriter.create<stablehlo::UniformQuantizeOp>(
+                op->getLoc(), oldResult.getType(), newResult));
+      } else {
+        quantizedResults.push_back(newResult);
+      }
+    }
+    rewriter.replaceOp(op, quantizedResults);
+    return success();
+  }
+};
 
 class StablehloLegalizeQuantizedOpToQDQPass
     : public impl::StablehloLegalizeQuantizedOpToQDQPassBase<
@@ -147,45 +106,29 @@ class StablehloLegalizeQuantizedOpToQDQPass
   GreedyRewriteConfig config;
 };
 
+template <typename... StablehloOpTypes>
+void populateStablehloLegalizeQuantizedOpToQDQPatterns(
+    RewritePatternSet* patterns, MLIRContext* context) {
+  patterns->add<QuantizedStablehloOpConversion<StablehloOpTypes>...>(context);
+}
+
 }  // namespace
 
 void populateStablehloLegalizeQuantizedOpToQDQPatterns(
     RewritePatternSet* patterns, MLIRContext* context) {
-  patterns->add<QuantizedAbsOpConversion>(context);
-  patterns->add<QuantizedAtan2OpConversion>(context);
-  patterns->add<QuantizedBatchNormGradOpConversion>(context);
-  patterns->add<QuantizedBatchNormInferenceOpConversion>(context);
-  patterns->add<QuantizedBatchNormTrainingOpConversion>(context);
-  patterns->add<QuantizedCbrtOpConversion>(context);
-  patterns->add<QuantizedCeilOpConversion>(context);
-  patterns->add<QuantizedCholeskyOpConversion>(context);
-  patterns->add<QuantizedClampOpConversion>(context);
-  patterns->add<QuantizedCompareOpConversion>(context);
-  patterns->add<QuantizedCosineOpConversion>(context);
-  patterns->add<QuantizedDivOpConversion>(context);
-  patterns->add<QuantizedExpm1OpConversion>(context);
-  patterns->add<QuantizedExpOpConversion>(context);
-  patterns->add<QuantizedFloorOpConversion>(context);
-  patterns->add<QuantizedLog1pOpConversion>(context);
-  patterns->add<QuantizedLogisticOpConversion>(context);
-  patterns->add<QuantizedLogOpConversion>(context);
-  patterns->add<QuantizedMaxOpConversion>(context);
-  patterns->add<QuantizedMinOpConversion>(context);
-  patterns->add<QuantizedMulOpConversion>(context);
-  patterns->add<QuantizedNegOpConversion>(context);
-  patterns->add<QuantizedPowOpConversion>(context);
-  patterns->add<QuantizedReducePrecisionOpConversion>(context);
-  patterns->add<QuantizedRemOpConversion>(context);
-  patterns->add<QuantizedRoundOpConversion>(context);
-  patterns->add<QuantizedRoundNearestEvenOpConversion>(context);
-  patterns->add<QuantizedRsqrtOpConversion>(context);
-  patterns->add<QuantizedSelectOpConversion>(context);
-  patterns->add<QuantizedSignOpConversion>(context);
-  patterns->add<QuantizedSineOpConversion>(context);
-  patterns->add<QuantizedSqrtOpConversion>(context);
-  patterns->add<QuantizedSubtractOpConversion>(context);
-  patterns->add<QuantizedTanhOpConversion>(context);
-  patterns->add<QuantizedTriangularSolveOpConversion>(context);
+  // The following list covers most of the operations which, according to the
+  // stablehlo spoecification document, interprets the quantized
+  // operation using dequant-op-quant strategy. The ones excluded are
+  // AddOP, ConvolutionOp, DotGeneralOp, and DynamicConvOp, which are current
+  // using `stablehlo-legalize-quant-to-int` pass for decomposituion to
+  // primitive math operations.
+  populateStablehloLegalizeQuantizedOpToQDQPatterns<
+      AbsOp, Atan2Op, BatchNormGradOp, BatchNormInferenceOp,
+      BatchNormTrainingOp, CbrtOp, CeilOp, CholeskyOp, ClampOp, CompareOp,
+      CosineOp, DivOp, Expm1Op, ExpOp, FloorOp, Log1pOp, LogisticOp, LogOp,
+      MaxOp, MinOp, MulOp, NegOp, PowOp, ReducePrecisionOp, RemOp, RoundOp,
+      RoundNearestEvenOp, RsqrtOp, SelectOp, SignOp, SineOp, SqrtOp, SubtractOp,
+      TanhOp, TriangularSolveOp>(patterns, context);
 }
 
 }  // namespace stablehlo
