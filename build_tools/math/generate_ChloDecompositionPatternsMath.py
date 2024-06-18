d41390c3a731ba038e6363f75fcd135e6f727039
Sandeep Dasgupta <sdasgup@google.com>
2024-06-18 20:38:15 +0000

    Lint fixes driven by internal intregation (#2399)

--- build_tools/math/generate_ChloDecompositionPatternsMath.py
+++ build_tools/math/generate_ChloDecompositionPatternsMath.py
@@ -22,54 +22,64 @@ import warnings
 
 
 def main():
-    try:
-        import functional_algorithms as fa
-    except ImportError as msg:
-        print(f"Skipping: {msg}")
-        return
+  try:
+    import functional_algorithms as fa
+  except ImportError as msg:
+    print(f"Skipping: {msg}")
+    return
 
-    fa_version = tuple(map(int, fa.__version__.split(".", 4)[:3]))
-    if fa_version < (0, 4, 0):
-        warnings.warn("functional_algorithm version 0.4.0 or newer is required,"
-                      f" got {fa.__version__}")
-        return
+  fa_version = tuple(map(int, fa.__version__.split(".", 4)[:3]))
+  if fa_version < (0, 4, 0):
+    warnings.warn(
+        "functional_algorithm version 0.4.0 or newer is required,"
+        f" got {fa.__version__}"
+    )
+    return
 
-    output_file = os.path.relpath(
-        os.path.normpath(
-            os.path.join(os.path.dirname(__file__), "..", "..", "stablehlo",
-                         "transforms", "ChloDecompositionPatternsMath.td")),
-        os.getcwd())
+  output_file = os.path.relpath(
+      os.path.normpath(
+          os.path.join(
+              os.path.dirname(__file__),
+              "..",
+              "..",
+              "stablehlo",
+              "transforms",
+              "ChloDecompositionPatternsMath.td",
+          )
+      ),
+      os.getcwd(),
+  )
 
-    sources = []
-    target = fa.targets.stablehlo
-    for chloname, fname, args in [
-        ("CHLO_AsinOp", "complex_asin", ("z:complex",)),
-        ("CHLO_AsinOp", "real_asin", ("x:float",)),
-    ]:
-        func = getattr(fa.algorithms, fname, None)
-        if func is None:
-            warnings.warn(
-                "{fa.algorithms.__name__} does not define {fname}. Skipping.")
-            continue
-        ctx = fa.Context(paths=[fa.algorithms])
-        graph = ctx.trace(func, *args).implement_missing(target).simplify()
-        graph.props.update(name=chloname)
-        src = graph.tostring(target)
-        sources.append(target.make_comment(
-            func.__doc__)) if func.__doc__ else None
-        sources[-1] += src
-    source = "\n\n".join(sources) + "\n"
+  sources = []
+  target = fa.targets.stablehlo
+  for chloname, fname, args in [
+      ("CHLO_AsinOp", "complex_asin", ("z:complex",)),
+      ("CHLO_AsinOp", "real_asin", ("x:float",)),
+  ]:
+    func = getattr(fa.algorithms, fname, None)
+    if func is None:
+      warnings.warn(
+          "{fa.algorithms.__name__} does not define {fname}. Skipping."
+      )
+      continue
+    ctx = fa.Context(paths=[fa.algorithms])
+    graph = ctx.trace(func, *args).implement_missing(target).simplify()
+    graph.props.update(name=chloname)
+    src = graph.tostring(target)
+    sources.append(target.make_comment(func.__doc__)) if func.__doc__ else None
+    sources[-1] += src
+  source = "\n\n".join(sources) + "\n"
 
-    if os.path.isfile(output_file):
-        f = open(output_file, "r")
-        content = f.read()
-        f.close()
-        if content.endswith(source):
-            print(f"{output_file} is up-to-date.")
-            return
+  if os.path.isfile(output_file):
+    f = open(output_file, "r")
+    content = f.read()
+    f.close()
+    if content.endswith(source):
+      print(f"{output_file} is up-to-date.")
+      return
 
-    f = open(output_file, "w")
-    f.write("""\
+  f = open(output_file, "w")
+  f.write("""\
 /* Copyright 2024 The StableHLO Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -86,15 +96,14 @@ limitations under the License.
 ==============================================================================*/
 
 """)
-    f.write(
-        target.make_comment(f"""\
+  f.write(target.make_comment(f"""\
 
 This file is generated using functional_algorithms tool ({fa.__version__}).
 See build_tools/math/README.md for more information.""") + "\n")
-    f.write(source)
-    f.close()
-    print(f"Created {output_file}")
+  f.write(source)
+  f.close()
+  print(f"Created {output_file}")
 
 
 if __name__ == "__main__":
-    main()
+  main()
