c80cf2008d18b21a168b9c66ab3dd9585f9fb0da
Abhinav <agunjal@google.com>
2024-07-02 17:51:42 +0000

    removed extra new lines

--- stablehlo/dialect/VhloOps.cpp
+++ stablehlo/dialect/VhloOps.cpp
@@ -334,7 +334,8 @@ LogicalResult verifyConstraint_0_17_0(mlir::Operation* op,
   return success();
 }
 
-// Allow DictionaryAttr at custom_call backend_config for versions >= v1.1.0
+// Allow Dictionary attribute at custom_call backend_config,
+// for versions >= "1.1.0"
 LogicalResult verifyConstraint_1_1_0(mlir::Operation* op,
                                      Version targetVersion) {
   auto customCallOp = dyn_cast<mlir::vhlo::CustomCallOpV1>(op);
