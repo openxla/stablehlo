f3bba04069f7545c91b5d3c2fc7eedf20c0d7310
Sandeep Dasgupta <sdasgup@google.com>
2024-07-23 20:17:51 +0000

    fix tests

--- stablehlo/transforms/StablehloLegalizeQuantToMath.cpp
+++ stablehlo/transforms/StablehloLegalizeQuantToMath.cpp
@@ -65,10 +65,6 @@ bool isPerAxisType(Type type) {
   return isa<quant::UniformQuantizedPerAxisType>(getElementTypeOrSelf(type));
 }
 
-// bool isQuantType(Type type) {
-//   return isPerTensorType(type) || isPerAxisType(type);
-// }
-
 quant::UniformQuantizedType getPerTensorType(Type type) {
   return cast<quant::UniformQuantizedType>(getElementTypeOrSelf(type));
 }
@@ -990,8 +986,7 @@ LogicalResult matchAndRewriteDotLikeOp(DotLikeOp op, DotLikeOpAdaptor adaptor,
 }
 
 template <typename DotLikeOp>
-FailureOr<bool> isDotLikeOpHybrid(DotLikeOp op,
-                                  ConversionPatternRewriter &rewriter) {
+FailureOr<bool> isDotLikeOpHybrid(DotLikeOp op) {
   // Checks whether a dot-like op is hybrid by looking at input/output types.
   // Returns failure() when the type is not supported.
   bool isLhsQuant = isa<quant::UniformQuantizedType>(
@@ -1019,8 +1014,6 @@ FailureOr<bool> isDotLikeOpHybrid(DotLikeOp op,
   }
   op->emitError("Invalid input/output type for Dot/Convolution op");
   return failure();
-  // return rewriter.notifyMatchFailure(op, "Invalid input/output type for
-  // Dot/Convolution op");
 }
 
 class ConvertUniformQuantizedDotOp
@@ -1031,7 +1024,7 @@ class ConvertUniformQuantizedDotOp
   LogicalResult matchAndRewrite(
       stablehlo::DotOp op, stablehlo::DotOpAdaptor adaptor,
       ConversionPatternRewriter &rewriter) const override {
-    auto isHybrid = isDotLikeOpHybrid(op, rewriter);
+    auto isHybrid = isDotLikeOpHybrid(op);
     if (failed(isHybrid)) {
       return failure();
     }
@@ -1067,7 +1060,7 @@ class ConvertUniformQuantizedDotGeneralOp
   LogicalResult matchAndRewrite(
       stablehlo::DotGeneralOp op, stablehlo::DotGeneralOpAdaptor adaptor,
       ConversionPatternRewriter &rewriter) const override {
-    auto isHybrid = isDotLikeOpHybrid(op, rewriter);
+    auto isHybrid = isDotLikeOpHybrid(op);
     if (failed(isHybrid)) {
       return failure();
     }
@@ -1216,7 +1209,7 @@ class ConvertUniformQuantizedConvolutionOp
   LogicalResult matchAndRewrite(
       stablehlo::ConvolutionOp op, stablehlo::ConvolutionOpAdaptor adaptor,
       ConversionPatternRewriter &rewriter) const override {
-    auto isHybrid = isDotLikeOpHybrid(op, rewriter);
+    auto isHybrid = isDotLikeOpHybrid(op);
     if (failed(isHybrid)) {
       return failure();
     }
@@ -1384,17 +1377,10 @@ class StablehloLegalizeQuantToMathPass
     patterns.add<ConvertUniformQuantizedAddOp, ConvertUniformQuantizedDotOp,
                  ConvertUniformQuantizedDotGeneralOp,
                  ConvertUniformQuantizedConvolutionOp>(context, /*benefit=*/10);
-<<<<<<< HEAD
 
     // Populate stablehlo quant-op to dq-op-q patterns as fallback.
     populateStablehloLegalizeQuantizedOpToQDQPatterns(&patterns, context,
                                                       /*benefit=*/1);
-=======
-    populateStablehloLegalizeQuantizedOpToQDQPatterns(patterns, context,
-                                                      /*benefit=*/0);
-    patterns.add<ConvertUniformQuantizeOp, ConvertUniformDequantizeOp>(
-        context, /*benefit=*/0);
->>>>>>> a52cb9b6 (Using benefits as opposed to source/target materialization)
 
     // uq->int convert patterns for func.func, func.return and generic ops.
     UniformQuantizedToIntTypeConverter converter;
