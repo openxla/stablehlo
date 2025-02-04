5ff5236e00e38d996e14764e983e59901cc55fab
Kevin Gleason <gleasonk@google.com>
2025-02-04 22:23:20 +0000

    Address feedback

--- stablehlo/transforms/optimization/StablehloTargetIndependentOptimization.cpp
+++ stablehlo/transforms/optimization/StablehloTargetIndependentOptimization.cpp
@@ -1,10 +1,16 @@
-// Copyright 2023 The IREE Authors
-//
-// Licensed under the Apache License, Version 2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+/* Copyright 2025 The StableHLO Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-// Implements optional canonicalization patterns for StableHLO ops.
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
 
 #include <cassert>
 #include <cstdint>
