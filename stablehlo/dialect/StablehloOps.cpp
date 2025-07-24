0ad564c9ea569ba07fe14d224336b11eda3b77a1
mrguenther <mrguenther@google.com>
2025-07-24 11:37:31 -0700

    Integrate; skip LLVM bump due to upstream issue.

--- stablehlo/dialect/StablehloOps.cpp
+++ stablehlo/dialect/StablehloOps.cpp
@@ -2201,12 +2201,14 @@ void ReduceWindowOp::build(
   locs.reserve(numValues);
   for (auto i : inputs) {
     auto iType = cast<ShapedType>(i.getType());
-    blockArgTypes.push_back(iType.cloneWith({}, iType.getElementType()));
+    blockArgTypes.push_back(iType.cloneWith(
+        llvm::ArrayRef<int64_t>(), iType.getElementType()));
     locs.push_back(i.getLoc());
   }
   for (auto i : init_values) {
     auto iType = cast<ShapedType>(i.getType());
-    blockArgTypes.push_back(iType.cloneWith({}, iType.getElementType()));
+    blockArgTypes.push_back(iType.cloneWith(
+        llvm::ArrayRef<int64_t>(), iType.getElementType()));
     locs.push_back(i.getLoc());
   }
 
@@ -3084,7 +3086,6 @@ StablehloDialect::StablehloDialect(MLIRContext* context)
   addOperations<
 #define GET_OP_LIST
 #include "stablehlo/dialect/StablehloOps.cpp.inc"
-
       >();
   addInterfaces<StablehloDialectInlinerInterface>();
   addInterfaces<StablehloHloDialectInterface>();
@@ -3093,7 +3094,6 @@ StablehloDialect::StablehloDialect(MLIRContext* context)
   addAttributes<
 #define GET_ATTRDEF_LIST
 #include "stablehlo/dialect/StablehloAttrs.cpp.inc"
-
       >();
 }
 
