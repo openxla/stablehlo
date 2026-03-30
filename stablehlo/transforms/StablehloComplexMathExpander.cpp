b38fb48919d3ed4e21835b3167ba43f300529acb
ddilbaz <ddilbaz@tenstorrent.com>
2026-03-30 20:28:31 +0000

    Complex math expander full vs default - need to create a new td file after this commit

--- stablehlo/transforms/StablehloComplexMathExpander.cpp
+++ stablehlo/transforms/StablehloComplexMathExpander.cpp
@@ -1,14 +1,4 @@
-/* Copyright 2024 The StableHLO Authors. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+/* Copyright 2026 The StableHLO Authors. */
 
 #include <utility>
 
@@ -19,67 +9,79 @@ limitations under the License.
 
 namespace mlir {
 namespace stablehlo {
+
+// --- 1. Helper Functions (Must be in mlir::stablehlo for TableGen) ---
+
+Value getConstantLikeMaxFiniteValue(OpBuilder &b, Location loc, Value val) {
+  auto ty = cast<FloatType>(getElementTypeOrSelf(val.getType()));
+  return getConstantLike(b, loc, llvm::APFloat::getLargest(ty.getFloatSemantics()), val);
+}
+
+Value getConstantLikeInfValue(OpBuilder &b, Location loc, Value val, bool negative) {
+  auto ty = cast<FloatType>(getElementTypeOrSelf(val.getType()));
+  return getConstantLike(b, loc, llvm::APFloat::getInf(ty.getFloatSemantics(), negative), val);
+}
+
+// --- 2. Forward Declarations for Population ---
+
+void populateStablehloComplexMathExpanderPatterns(MLIRContext *context,
+                                                  RewritePatternSet *patterns);
+void populateStablehloComplexFullMathExpanderPatterns(MLIRContext *context,
+                                                      RewritePatternSet *patterns);
+
 #define GEN_PASS_DEF_STABLEHLOCOMPLEXMATHEXPANDERPASS
 #include "stablehlo/transforms/Passes.h.inc"
 
 namespace {
 
-static Value getConstantLikeMaxFiniteValue(OpBuilder &b, Location loc,
-                                           Value val) {
-  auto ty = cast<FloatType>(getElementTypeOrSelf(val.getType()));
-  return getConstantLike(
-      b, loc, llvm::APFloat::getLargest(ty.getFloatSemantics()), val);
-}
-
-static Value getConstantLikeInfValue(OpBuilder &b, Location loc, Value val,
-                                     bool negative) {
-  auto ty = cast<FloatType>(getElementTypeOrSelf(val.getType()));
-  return getConstantLike(
-      b, loc, llvm::APFloat::getInf(ty.getFloatSemantics(), negative), val);
-}
-
-//===----------------------------------------------------------------------===//
-// Pass
-//===----------------------------------------------------------------------===//
-
-struct StablehloComplexMathExpanderPass
-    : public impl::StablehloComplexMathExpanderPassBase<
-          StablehloComplexMathExpanderPass> {
-  StablehloComplexMathExpanderPass()
-      : StablehloComplexMathExpanderPassBase<
-            StablehloComplexMathExpanderPass>() {}
-
- public:
-  LogicalResult initialize(MLIRContext *context) override {
-    config.setUseTopDownTraversal(true);
-    RewritePatternSet patterns_(context);
-    populateStablehloComplexMathExpanderPatterns(context, &patterns_);
-    patterns = std::move(patterns_);
-    return success();
-  }
-
-  void runOnOperation() override {
-    auto func = getOperation();
-    if (failed(applyPatternsGreedily(func, patterns, config))) {
-      func.emitError("Failed to converge StableHLOComplexMathExpanderPass in ")
-          << config.getMaxIterations() << " iterations";
-      signalPassFailure();
-    }
-  }
-
- private:
-  FrozenRewritePatternSet patterns;
-  GreedyRewriteConfig config;
-};
+// --- 3. Include Patterns (Generated code uses helpers above) ---
 
 #include "stablehlo/transforms/StablehloComplexMathExpanderPatterns.h.inc"
 
-}  // namespace
+// --- 4. Pass Implementation ---
+
+struct StablehloComplexMathExpanderPass
+    : public impl::StablehloComplexMathExpanderPassBase<StablehloComplexMathExpanderPass> {
+  
+  // Use the Base class constructor to properly initialize 'enableFullExpansion'
+  using StablehloComplexMathExpanderPassBase::StablehloComplexMathExpanderPassBase;
+
+  void runOnOperation() override {
+    auto func = getOperation();
+    MLIRContext *context = &getContext();
+    RewritePatternSet patterns(context);
+
+    // Default accuracy patterns
+    populateStablehloComplexMathExpanderPatterns(context, &patterns);
+
+    // Full expansion (Mul/Div) logic
+    if (enableFullExpansion) {
+      populateStablehloComplexFullMathExpanderPatterns(context, &patterns);
+    }
+
+    if (failed(applyPatternsGreedily(func, std::move(patterns)))) {
+      func.emitError("Failed to converge StableHLOComplexMathExpanderPass");
+      signalPassFailure();
+    }
+  }
+};
+
+} // namespace
+
+// --- 5. Definition of Population Functions ---
 
 void populateStablehloComplexMathExpanderPatterns(MLIRContext *context,
                                                   RewritePatternSet *patterns) {
+  // Logic to populate standard patterns
   populateWithGenerated(*patterns);
 }
 
-}  // namespace stablehlo
-}  // namespace mlir
+void populateStablehloComplexFullMathExpanderPatterns(MLIRContext *context,
+                                                      RewritePatternSet *patterns) {
+  // If you split the .td files later, this would call populateFullWithGenerated.
+  // For now, it shares the same generated pool.
+  populateWithGenerated(*patterns);
+}
+
+} // namespace stablehlo
+} // namespace mlir
