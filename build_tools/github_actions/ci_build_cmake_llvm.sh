f026fa20c8cf21aa882b3029c501d2c81da7b008
mrguenther <mrguenther@google.com>
2026-05-07 15:58:49 -0700

    Upgrade to Python 3.12

--- build_tools/github_actions/ci_build_cmake_llvm.sh
+++ build_tools/github_actions/ci_build_cmake_llvm.sh
@@ -58,7 +58,7 @@ cmake -GNinja \
   -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
   -DCMAKE_C_COMPILER=clang \
   -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-  -DPython3_EXECUTABLE=/usr/bin/python3.11
+  -DPython3_EXECUTABLE=/usr/bin/python3.12
 
 # Build LLVM/MLIR
 cmake --build "$LLVM_BUILD_DIR" --target all
